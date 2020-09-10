e0995d04a24c35776d77e8e2f2367833ebdb1c38
mooncreat <42803973+mooncreat@users.noreply.github.com>
2020-09-10 16:18:03 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+</head>
+<body>
+    <h1>JavaScript</h1>
+    <script>
+        document.write("10"+"10");
+    </script>
+</body>
+</html>
